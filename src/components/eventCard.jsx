0be4a5a8a03623f37f50e6bc73ea711d0b8df39d
Nikunj Sharma <168582499+Nicksharmaaa@users.noreply.github.com>
2025-07-14 18:43:13 +0530

    initial commit

---
+++ src/components/eventCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const eventCard = () => {
+  return (
+    <div>eventCard</div>
+  )
+}
+
+export default eventCard
---
+++ src/components/eventCardModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const eventCardModal = () => {
+  return (
+    <div>eventCardModal</div>
+  )
+}
+
+export default eventCardModal
