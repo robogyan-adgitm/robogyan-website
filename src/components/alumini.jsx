5be2cca7132866fa3a37df7b6471379824f4f750
AchintyaBhardwaj <achi.bhardwaj@gmail.com>
2025-07-17 21:13:37 +0530

    Alumini/Members cards added

--- src/components/alumini.jsx
+++ src/components/alumini.jsx
@@ -3,8 +3,134 @@ import React from 'react'
 const AluminiCard = () => {
   return (
     <>
-      <h1>sdsda</h1>
-      <h2>jnadjsn</h2>
+    <div class="bg-[#000000]">
+      <div class="text-center text-4xl font-bold mt-4 text-white">Meet the team</div>
+      <div class="text-center mt-20 text-xl font-bold text-white">Meet the team who lead the society to new heights </div>
+      <div class="flex justify-center space-x-10 sm:grid-cols-1 lg:grid-cols-2">
+        <div class="mt-8 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-8 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-40">   
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github (1).png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      <div class="mt-10 w-65 h-90 bg-[#333333] rounded-xl ">
+        <img src ="logo.jpeg" alt="Random image" class="rounded-full w-35 h-35 mt-5 border-1 mx-auto block border-[#ffebaf]"></img>
+        <h1 class="text-white text-xl text-center font-bold mt-2 font-sans-serif">Amogh Saxena</h1>
+        <h3 class="text-lg text-center text-[#ffebaf] font-serif">President</h3>
+        <p class="text-white text-sm text-center p-4">Third year undergraduate in computer science</p>
+        <div class="flex space-x-10 justify-center mt-6 ">
+          <img src ="github.png" alt="Github image" class="w-5"></img>
+          <img src ="linkedin.png" alt="Linkedin image" class="w-5"></img>
+          <img src ="gmail.png" alt="Gmail image" class="w-5"></img>
+          <img src ="instagram.png" alt="Instagram image" class="w-5"></img>
+        </div>
+      </div>
+      </div>
+    </div>  
     </>
     
   )
